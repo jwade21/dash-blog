6a726d338e0c447f0c64743681d0973f9126db9c
jordan wade <jordanwade2009@live.com>
2016-11-23 00:07:56 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#dislike").on("click", function() {
+  alert("No one cares what you think.");
+  });
+
+
+  $("#notify").on("click", function() {
+  alert("NOTICE:\nThank you for noticing this new notice in order to notify the 'Notifyers'. Your noticing it has been noticed.\nAnd will be reported to the authorites.");
+  });
+
+  $('#serious').click(function() {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+  });
+});
